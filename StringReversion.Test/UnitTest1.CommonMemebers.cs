5b3703326de750219556b695381947fe69b40064
kuritka <kuritka@gmail.com>
2014-11-10 00:28:19 +0100

    Reversion Words in string
    "Life is short. Buy the shoes!" reverts to   "shoes! the Buy short. is
    Life"

---
+++ StringReversion.Test/UnitTest1.CommonMemebers.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace StringReversion.Test
+{
+    public partial class UnitTest1
+    {
+
+        private const string Sentence = "Life is short. Buy the shoes!";
+        private const string RevertedSentence = "shoes! the Buy short. is Life";
+
+        [TestFixtureSetUp]
+        public void Init()
+        {
+        }
+
+        [TestFixtureTearDown]
+        public void Dispose()
+        {
+        }
+
+
+        private static string Revert(string s)
+        {
+            if (s == null)
+            {
+                throw new ArgumentNullException();
+            }
+            var stack = new Stack();
+            var word = new StringBuilder();
+            foreach (var c in s)
+            {
+                if (!char.IsWhiteSpace(c))
+                {
+                    word.Append(c);
+                }
+                else
+                {
+                    if (word.Length > 0)
+                    {
+                        stack.Push(word.ToString());
+                        word.Clear();
+                    }
+                    stack.Push(c);
+                }
+            }
+            if (!string.IsNullOrEmpty(word.ToString()))
+            {
+                stack.Push(word);
+            }        
+            return string.Concat(stack.ToArray());
+        }
+
+
+
+
+
+      
+        private static string RevertLinq(string s)
+        {
+            if (s == null)
+            {
+                throw new ArgumentNullException();
+            }
+            var reversedSplit = s.Split(' ').Reverse();
+            var stringBuilder = new StringBuilder(s.Length);
+            var i = reversedSplit.Count();
+            reversedSplit.ToList().ForEach(d =>
+            {
+                stringBuilder.Append(d);
+                if(--i > 0)
+                    stringBuilder.Append(' ');
+            });
+            return stringBuilder.ToString();
+            
+            //Cannot use aggregate, It doesnt internally use StringBuilder
+            //and it is extremelly slow !! 
+            //return 
+            //    new Stack<string>(s.Split(' ')).ToArray().Aggregate( (x,y) => x + " " +y );
+        }
+
+
+
+        private string GetLongString(string value, int count = 5000000)
+        {
+            var stringBuilder = new StringBuilder();
+            for (var i = 1; i <= count; i++)
+            {
+                stringBuilder.Append(value);
+                if (i < count)
+                {
+                    stringBuilder.Append(' ');
+                }
+            }
+            return stringBuilder.ToString();
+        }
+
+    }
+}
---
+++ StringReversion.Test/UnitTest1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace StringReversion.Test
+{
+
+    //Various cultures or charsets - no, it doesnt require test
+    //As Async - no, because async is suitable for non-locking operations like I/O, service calls etc. This is blocking op.
+    //
+    [TestFixture]
+    partial class UnitTest1
+    {
+
+        [Test]
+        public void CorrectSentense()
+        {
+            Revert(Sentence).Should().Be(RevertedSentence);
+        }
+
+        [Test]
+        public void CorrectSentenseWithEmptyString()
+        {
+            Revert(string.Empty).Should().Be(string.Empty);
+        }
+
+        [Test]
+        public void CorrectSentenseWithNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => Revert(null));
+        }
+
+        [Test]
+        public void CorrectSentenseWithSacesBehind()
+        {    
+            const string space = "       ";
+            const string sentence = Sentence + space;
+            Revert(sentence).Should().Be(space + RevertedSentence);
+        }
+
+        [Test]
+        public void CorrectSentenseWithSacesAbove()
+        {
+            const string space = "       ";
+            const string sentence = space + Sentence;
+            Revert(sentence).Should().Be(RevertedSentence + space);
+        }
+
+
+        [Test]
+        public void CorrectSentenseWithBigData()
+        {
+            var t = DateTime.Now;
+            var longString = GetLongString("Hello");
+            Revert(longString).Should().Be(longString);
+            Console.WriteLine("{0:c} seconds", DateTime.Now - t); 
+        }
+
+        [Test]
+        public void CorrectSentenseWithLinq()
+        {
+          RevertLinq(Sentence).Should().Be(RevertedSentence);
+        }
+
+
+        [Test]
+        public void CorrectSentenseWithBigDataLinq()
+        {
+            var t = DateTime.Now;
+            var longString = GetLongString("Hello");
+            RevertLinq(longString).Should().Be(longString);
+            Console.WriteLine("{0:c} seconds", DateTime.Now - t); 
+        }
+
+
+        [Test]
+        public void CorrectSentenseWithEmptyStringLinq()
+        {
+            Revert(string.Empty).Should().Be(string.Empty);
+        }
+
+        [Test]
+        public void CorrectSentenseWithNullLinq()
+        {
+            Assert.Throws<ArgumentNullException>(() => RevertLinq(null));
+        }
+    }
+}
